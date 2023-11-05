652c1b83a5b709f3f5a2de93b70865315b2e166a
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-11-05 19:42:20 +0300

    add random cardList

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,19 +2,20 @@ import React from "react";
 import "./Card.css";
 
 type Props = {
-  url: string;
+  url: string,
+  id: number
 };
 
 export const Card = (props: Props) => {
-  const { url } = props;
+  const { url, id } = props;
   const [isCardOpen, setIsCardOpen] = React.useState(false);
-  const cardClassName: string = `card ${
-    isCardOpen ? "card_front" : "card_back"
-  }`;
+  //   const cardClassName: string = `card ${
+  //     isCardOpen ? "card_front" : "card_back"
+  //   }`;
   return (
     <div
-      className={cardClassName}
-    // className="card card_back"
+      //   className={cardClassName}
+      className="card card_back"
       onClick={() => {
         setIsCardOpen(!isCardOpen);
       }}
