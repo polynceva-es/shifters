3fbc312582fa25a87f2faff03fc04259eeb02dc7
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-11-17 12:30:23 +0300

    add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import { FC } from 'react';
-import { useSelector } from 'react-redux';
+import { FC } from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import { Cards } from "./components/Cards/Cards";
-import { Popup } from './components/Popup/Popup';
-import { RootState } from './services/store';
+import { Popup } from "./components/Popup/Popup";
+import { RootState } from "./services/store";
 
 const App: FC = () => {
   const isPopupOpen = useSelector((state: RootState) => state.popup.isOpen);
 
   return (
     <>
-    <h1 id="title">Shifters</h1>
-    <Popup />
-    {!isPopupOpen ? <Cards /> : <></>}
-  </>
+      <h1 id="title">Shifters</h1>
+      <Popup />
+      {!isPopupOpen ? <Cards /> : <></>}
+      <footer>
+        <a href="https://github.com/polynceva-es/" target="_blanck">
+          development by Ekaterina Polyntseva
+        </a>
+      </footer>
+    </>
   );
-}
+};
 
 export default App;
