1021cd19ae6405a0b833475995745d6d93a6bb39
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2025-03-09 13:27:21 +0300

    add Footer

--- src/pages/shifters/Shifters.tsx
+++ src/pages/shifters/Shifters.tsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { Cards } from "../../components/Cards/Cards";
 import { Popup } from "../../components/Popup/Popup";
 import { RootState } from "../../services/store";
-import { Link } from "react-router-dom";
 
 export const Shifters: FC = () => {
   const isPopupOpen = useSelector((state: RootState) => state.popup.isOpen);
@@ -13,14 +12,6 @@ export const Shifters: FC = () => {
       <h1 id="title">Shifters</h1>
       <Popup />
       {!isPopupOpen ? <Cards /> : <></>}
-      <Link to="/shifters" className="home__link">
-        Home
-      </Link>
-      <footer>
-        <a href="https://github.com/polynceva-es/" target="_blanck">
-          development by Ekaterina Polyntseva
-        </a>
-      </footer>
     </>
   );
 };
--- src/pages/ticTacToe/TicTacToe.tsx
+++ src/pages/ticTacToe/TicTacToe.tsx
@@ -2,7 +2,6 @@ import { FC } from "react";
 import { useDispatch } from "react-redux";
 import { PlayList } from "../../components/PlayList/PlayList.tsx";
 import { handleStartAgain } from "../../services/redusers/playSlice.tsx";
-import { Link } from "react-router-dom";
 
 export const TicTacToe: FC = () => {
   const dispatch = useDispatch();
@@ -13,9 +12,6 @@ export const TicTacToe: FC = () => {
     <>
       <PlayList />
       <button className='button' onClick={() => startAgain()} content="&nbsp;Start&nbsp;again&nbsp;">&nbsp;Start&nbsp;again&nbsp;</button>
-      <Link to="/shifters" className="home__link">
-        Home
-      </Link>
     </>
   );
 };
