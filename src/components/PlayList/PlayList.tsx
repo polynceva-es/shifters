5ca84e418884d4dbbb00aa5dcc7421882663a8b8
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2025-03-11 20:24:23 +0300

    finish

--- src/components/PlayList/PlayList.tsx
+++ src/components/PlayList/PlayList.tsx
@@ -1,17 +1,25 @@
 import "./PlayList.css";
 import { PlayItem } from "../PlayItem/PlayItem.js";
 import { numbers } from "../../const/const.js";
+import { useSelector } from "react-redux";
+import { RootState } from "../../services/store.tsx";
 
 export const PlayList = () => {
+  const isWin = useSelector((state: RootState) => state.play.win);
+    const winner = useSelector((state: RootState) => state.play.winner);
+    const move = useSelector((state: RootState) => state.play.move)
   return (
-    <ul className="playList">
-      {numbers.map((num, i) => {
-        return (
-          <li key={i}>
-            <PlayItem number={num} />
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul className="playList">
+        {numbers.map((num, i) => {
+          return (
+            <li key={i}>
+              <PlayItem number={num} />
+            </li>
+          );
+        })}
+      </ul>
+      {isWin ? <div>The winner is: {winner}</div> : <div>Next move: {move}</div>}
+    </>
   );
 };
--- src/const/const.tsx
+++ src/const/const.tsx
@@ -1,11 +1,2 @@
-export const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-export const win: number[][] = [
-  [1, 4, 7],
-  [2, 5, 8],
-  [3, 6, 9],
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [1, 5, 9],
-  [3, 5, 7],
-];
+export const numbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
